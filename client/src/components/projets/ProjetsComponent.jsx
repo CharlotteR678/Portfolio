725d897abafbbf82f313268dcc6203d66ac90bc4
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-07-18 15:29:15 +0200

    transformed sills string into array

--- client/src/components/projets/ProjetsComponent.jsx
+++ client/src/components/projets/ProjetsComponent.jsx
@@ -1,33 +1,31 @@
 import PropTypes from "prop-types";
 
-export default function ProjetsComponent({ project, image }) {
+export default function ProjetsComponent({ project }) {
   return (
     <>
-    <div className="projectImageContainter">
-      <img src={image} alt={project.title} className="projectImage" />
+      <div className="projectImageContainter">
+        <img src={project.image} alt={project.title} className="projectImage" />
       </div>
       <h2 className="h2Project">{project.title}</h2>
       <p className="projectDescription">{project.description}</p>
-      <p className="projectSkill">{project.skills}</p>
-      {/* <ul className="projectSkillMap">
+      <ul className="projectSkillMap">
         {project.skills.map((skill) => (
-          <li key={skill.id} className="projectSkill">
-            {skill.skill}
+          <li key={skill} className="projectSkill">
+            {skill}
           </li>
         ))}
-      </ul> */}
+      </ul>
     </>
   );
 }
 
 ProjetsComponent.propTypes = {
-  image: PropTypes.string.isRequired,
   project: PropTypes.shape({
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
     skills: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number.isRequired,
         skill: PropTypes.string.isRequired,
       })
     ).isRequired,
--- client/src/pages/Projets.jsx
+++ client/src/pages/Projets.jsx
@@ -4,15 +4,28 @@ import "../css/projets/Projets.css";
 
 export default function Projets() {
   const projects = useLoaderData();
+
+  const projectSkills = projects.map((project) => ({
+    id: project.id,
+    title: project.title,
+    description: project.description,
+    image: project.image,
+    skills: project.skills.split(" "),
+  }));
+
   return (
     <main id="projectPage">
-    <ul id="projectsMap">
-      {projects.map((project) => (
-        <li key={project.id} className="projectList">
-          <ProjetsComponent project={project} image={project.image} />
-        </li>
-      ))}
-    </ul>
+      <ul id="projectsMap">
+        {projectSkills.map((project) => (
+          <li key={project.id} className="projectList">
+            <ProjetsComponent
+              project={project}
+              image={project.image}
+              skills={projectSkills}
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
